201f12b0354c512ac10740cfda29b6e3b30b2715
Luis Mayta <luis.mayta@gmail.com>
2018-07-12 14:47:26 -0500

    First Commit

---
+++ resources/views/templates/partials/css.blade.php
@@ -0,0 +1,6 @@
+		<link rel="stylesheet" href="{{ asset('css/global.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/structure.css') }}">
+		<link rel="stylesheet" href="{{ asset('css/be_style.css') }}" id='style-static'>
+		<link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+		<!-- Revolution Slider -->
+   		<link rel="stylesheet" href="{{ asset('plugins/rs-plugin/css/settings.css') }}">
---
+++ resources/views/templates/partials/js.blade.php
@@ -0,0 +1,6 @@
+		<script src="{{ asset('js/jquery-2.1.4.min.js') }}"></script>
+		<script src="{{ asset('js/mfn.menu.js') }}"></script>
+		<script src="{{ asset('js/jquery.plugins.js') }}"></script>
+		<script src="{{ asset('js/jquery.jplayer.min.js') }}"></script>
+		<script src="{{ asset('js/animations/animations.js') }}"></script>
+		<script src="{{ asset('js/scripts.js') }}"></script>
